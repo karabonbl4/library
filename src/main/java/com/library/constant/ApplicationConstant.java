60ec901c8de17d7b0cb0f026b41027a157898735
Lesha Korobko <karabonbl4@gmail.com>
2023-08-03 10:21:23 +0300

    replaced constant class to interface

--- src/main/java/com/library/constant/ApplicationConstant.java
+++ src/main/java/com/library/constant/ApplicationConstant.java
@@ -3,17 +3,17 @@ package com.library.constant;
 
 import java.time.format.DateTimeFormatter;
 
-public final class ApplicationConstant {
+public interface ApplicationConstant {
 
-    public final static String AUTHOR_IS_DELETED = "Author is deleted successfully!";
+    String AUTHOR_IS_DELETED = "Author is deleted successfully!";
 
-    public final static String BOOK_IS_DELETED = "Book is deleted successfully!";
+    String BOOK_IS_DELETED = "Book is deleted successfully!";
 
-    public final static String PUBLISHER_IS_DELETED = "Publisher is deleted successfully!";
+    String PUBLISHER_IS_DELETED = "Publisher is deleted successfully!";
 
-    public final static String ENTITY_NOT_FOUND = "Entity not found!";
+    String ENTITY_NOT_FOUND = "Entity not found!";
 
-    public final static String FIELD_NOT_PRESENT = "Required field is not present!";
+    String FIELD_NOT_PRESENT = "Required field is not present!";
 
-    public final static DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss");
+    DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd-MM-yyyy hh:mm:ss");
 }
